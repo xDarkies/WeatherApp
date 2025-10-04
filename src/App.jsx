5086454ab93ede4e8d5510101f89efeb04093e77
Madness <awronski16@gmail.com>
2025-10-04 11:46:26 +0200

    Updating App Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Button from './Button.jsx'
+import Header from './Header.jsx'
+import SideBar from './SideBar.jsx'
 
 function App() {
 
   return (
     <>
+    <Header></Header>
+    <SideBar></SideBar>
     </>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,11 @@
+import './Header.css'
+
 function Header(){
     return(
         <>
+            <div>
+                <h1>Weather App</h1>
+            </div>
         </>
     );
 }
